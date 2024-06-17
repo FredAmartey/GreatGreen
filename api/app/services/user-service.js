6e69eefb5ef595c0a5316442cd2c223cbf43bc61
Fred Amartey <amartey.f@northeastern.edu>
2024-06-16 22:56:01 -0400

    refactored and removed api folder

--- api/app/services/user-service.js
+++
@@ -1,6 +0,0 @@
-import Models from "../models/index.js";
-import genericService from "./generic-service.js";
-
-export default {
-  ...genericService(Models.User),
-}
--- api/config.sample.js
+++
@@ -1,3 +0,0 @@
-export const PASSCODE = "XXXX";
-export const ADMINS = ["admin@gg.org"];
-export const IMAGE_UPLOAD_URL = "XXXX";
